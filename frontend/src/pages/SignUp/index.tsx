0c4303a37bbfcf59a158c34759df41e4b317e758
regisfaria <rgisfaria@yahoo.com.br>
2020-12-16 23:51:11 -0300

    finished signup page

--- frontend/src/pages/SignUp/index.tsx
+++ frontend/src/pages/SignUp/index.tsx
@@ -1,5 +1,11 @@
 import React, { useCallback, useRef } from 'react';
-import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import {
+  FiArrowLeft,
+  FiMail,
+  FiUser,
+  FiLock,
+  FiFileText,
+} from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
@@ -21,6 +27,7 @@ import { Container, Content, AnimatedContent, Background } from './styles';
 interface SignUpFormData {
   name: string;
   email: string;
+  login: string;
   password: string;
 }
 
@@ -35,25 +42,28 @@ const SignUp: React.FC = () => {
         formRef.current?.setErrors({});
 
         const schema = Yup.object().shape({
-          name: Yup.string().required('Nome obrigatório'),
+          name: Yup.string().required('Name required'),
+          login: Yup.string().required('Username required'),
           email: Yup.string()
-            .required('E-mail obrigatório')
-            .email('Digite um e-mail válido'),
-          password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+            .required('E-mail required')
+            .email('Write a valid e-mail'),
+          password: Yup.string().min(6, 'Password must have at least 6 digits'),
           confirmPassword: Yup.string()
-            .oneOf([Yup.ref('password')], 'As senhas não conferem')
-            .required('As senhas não conferem'),
+            .oneOf([Yup.ref('password')], "Password don't match")
+            .required("Password don't match"),
         });
 
         await schema.validate(data, {
           abortEarly: false,
         });
 
-        const { name, email, password } = data;
+        const { name, email, login, password } = data;
 
         await api.post('/users', {
           name,
           email,
+          login,
+          type: 'user',
           password,
         });
 
@@ -93,29 +103,35 @@ const SignUp: React.FC = () => {
           <img src={logoImg} alt="CCCRM" />
 
           <Form ref={formRef} onSubmit={handleSubmit}>
-            <h1>Faça o seu Cadastro</h1>
+            <h1>Account data</h1>
 
-            <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
+            <Input name="name" icon={FiUser} type="text" placeholder="Name" />
+            <Input
+              name="login"
+              icon={FiFileText}
+              type="text"
+              placeholder="Username"
+            />
             <Input name="email" icon={FiMail} placeholder="E-mail" />
             <Input
               name="password"
               icon={FiLock}
               type="password"
-              placeholder="Senha"
+              placeholder="Password"
             />
             <Input
               name="confirmPassword"
               icon={FiLock}
               type="password"
-              placeholder="Confirme a senha"
+              placeholder="Confirm password"
             />
 
-            <Button type="submit">Cadastrar</Button>
+            <Button type="submit">SignUp</Button>
           </Form>
 
           <Link to="/">
             <FiArrowLeft />
-            Voltar para login
+            Back to login
           </Link>
         </AnimatedContent>
       </Content>
