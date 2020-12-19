5b1b9e7998b6c352c23c01f43b7ebb2ec44fda8b
regisfaria <rgisfaria@yahoo.com.br>
2020-12-19 16:37:10 -0300

    added leads tables. needs to style it and connect api

--- frontend/src/pages/Leads/styles.ts
+++ frontend/src/pages/Leads/styles.ts
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div``;
 
-export const Content = styled.main`
-  max-width: 1120px;
-  margin: 64px auto;
-  display: flex;
+export const Content = styled.div`
+  height: 40em;
+  width: 100%;
 `;
