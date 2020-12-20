8c2fe646cc31c8e815a36770074809f3113d570a
regisfaria <rgisfaria@yahoo.com.br>
2020-12-20 16:01:31 -0300

    added a new error file handler

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,11 +1,13 @@
 import 'reflect-metadata';
+import 'dotenv';
 
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
 import routes from '@shared/infra/http/routes';
-import AppError from '@shared/errors/AppError';
+
+import errorHandler from '@shared/middlewares/errorHandler';
 
 import '@shared/infra/typeorm';
 import '@shared/container';
@@ -16,26 +18,10 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use(
-  (err: Error, _request: Request, response: Response, _next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: 'error',
-        message: err.message,
-      });
-    }
+app.use(errorHandler);
 
-    // eslint-disable-next-line no-console
-    console.log(err);
-
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
-    });
-  },
-);
-
-app.listen(3333, () => {
-  // eslint-disable-next-line no-console
-  console.log('server started on port 3333');
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(
+    `🚀 ${process.env.ENVIRONMENT} server started at port ${process.env.SERVER_PORT} 🚀`,
+  );
 });
