97a20660129d2a22e2892c79d8b29a979e65c388
regisfaria <rgisfaria@yahoo.com.br>
2020-12-20 17:29:24 -0300

    finished this page for the moment

--- frontend/src/pages/Leads/index.tsx
+++ frontend/src/pages/Leads/index.tsx
@@ -1,40 +1,99 @@
-import React from 'react';
-import 'react-day-picker/lib/style.css';
+import React, { useEffect, useState } from 'react';
+
 import { DataGrid } from '@material-ui/data-grid';
 import AppHeader from '../../components/AppHeader';
+import NoRowsOverlay from './components/NoRowsOverlay';
+
+import api from '../../services/api';
 
 import { Container, Content } from './styles';
 
-const Leads: React.FC = () => {
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-    },
-  ];
+interface Lead {
+  id: string;
+  usdot: string;
+  entityType: string | null;
+  operatingStatus: string | null;
+  companyName: string | null;
+  fullName: string | null;
+  primaryAddress: string | null;
+  state: string | null;
+  zipCode: string | null;
+  altAddress: string | null;
+  altState: string | null;
+  altZipCode: string | null;
+  phoneNumber: string | null;
+  powerUnits: string | null;
+  drivers: string | null;
+  mcs150FormDate: string | null;
+  operationClassification: string | null;
+  carrierOperation: string | null;
+  cargoCarried: string | null;
+  email: string | null;
+  bipdInsuranceRequired: string | null;
+  cargoInsuranceRequired: string | null;
+  bondInsuranceRequired: string | null;
+  insuranceCarrier: string | null;
+  policySurety: string | null;
+  postedDate: string | null;
+  coverageFrom: string | null;
+  coverageTo: string | null;
+  effectiveDate: string | null;
+  cancellationDate: string | null;
+}
 
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+// https://material-ui.com/pt/api/data-grid/
+const Leads: React.FC = () => {
+  const [leads, setLeads] = useState<Lead[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    api.get('/leads').then(response => {
+      setLeads(response.data);
+      setIsLoading(false);
+    });
+  }, []);
+
+  const columns = [
+    { field: 'usdot', headerName: 'USDOT', width: 200 },
+    { field: 'entityType', headerName: 'Entity Type', width: 200 },
+    { field: 'operatingStatus', headerName: 'Operating Status', width: 200 },
+    { field: 'fullName', headerName: 'Full name', width: 200 },
+    { field: 'companyName', headerName: 'Company Name', width: 200 },
+    { field: 'primaryAddress', headerName: 'Primary Address', width: 200 },
+    { field: 'state', headerName: 'State', width: 200 },
+    { field: 'zipCode', headerName: 'Primary ZIP', width: 200 },
+    { field: 'phoneNumber', headerName: 'Phone', width: 200 },
+    { field: 'powerUnits', headerName: 'Power Units', width: 200 },
+    { field: 'drivers', headerName: 'Drivers', width: 200 },
+    { field: 'mcs150FormDate', headerName: 'MCS-150 FormDate', width: 200 },
+    {
+      field: 'operationClassification',
+      headerName: 'Operation Classification',
+      width: 200,
+    },
+    { field: 'cargoCarried', headerName: 'Cargo Carrier', width: 200 },
+    { field: 'altAddress', headerName: 'Secondary Address', width: 200 },
+    { field: 'altState', headerName: 'Secondary State', width: 200 },
+    { field: 'altZipCode', headerName: 'Secondary ZIP', width: 200 },
+    { field: 'email', headerName: 'E-mail', width: 200 },
+    {
+      field: 'bipdInsuranceRequired',
+      headerName: 'BIPD Insurance',
+      width: 200,
+    },
+    {
+      field: 'cargoInsuranceRequired',
+      headerName: 'Cargo Insurance',
+      width: 200,
+    },
+    { field: 'bondInsuranceRequired', headerName: 'Company Name', width: 200 },
+    { field: 'insuranceCarrier', headerName: 'Insurance Carrier', width: 200 },
+    { field: 'policySurety', headerName: 'Policy Surety', width: 200 },
+    { field: 'postedDate', headerName: 'Posted Date', width: 200 },
+    { field: 'coverageFrom', headerName: 'Coverage From', width: 200 },
+    { field: 'coverageTo', headerName: 'Coverage To', width: 200 },
+    { field: 'effectiveDate', headerName: 'Effective Date', width: 200 },
+    { field: 'cancellationDate', headerName: 'Cancellation Date', width: 200 },
   ];
 
   return (
@@ -43,10 +102,17 @@ const Leads: React.FC = () => {
 
       <Content>
         <DataGrid
-          rows={rows}
+          components={{
+            noRowsOverlay: NoRowsOverlay,
+          }}
+          rows={leads}
           columns={columns}
-          pageSize={5}
           checkboxSelection
+          pageSize={15}
+          autoHeight
+          loading={isLoading}
+          // Below is to render "/lead/:id"
+          // onRowClick={() => {}}
         />
       </Content>
     </Container>
--- frontend/src/pages/Leads/styles.ts
+++ frontend/src/pages/Leads/styles.ts
@@ -3,6 +3,68 @@ import styled from 'styled-components';
 export const Container = styled.div``;
 
 export const Content = styled.div`
-  height: 40em;
   width: 100%;
+
+  .MuiDataGrid-root {
+    font-size: 1em;
+    color: #f4ede8;
+    border: none;
+
+    button {
+      width: 3em;
+      height: 3em;
+
+      svg {
+        transition: color 0.225s;
+
+        width: 3em;
+        height: 3em;
+      }
+
+      &:hover {
+        svg {
+          color: #ba382f;
+        }
+      }
+    }
+  }
+
+  .MuiDataGrid-viewport {
+    font-size: 0.7em;
+  }
+
+  .MuiDataGrid-columnsContainer {
+    button {
+      width: 1em;
+      height: 1em;
+
+      svg {
+        width: 1em;
+        height: 1em;
+      }
+    }
+  }
+
+  .MuiDataGrid-colCellWrapper {
+    color: #ba382f;
+  }
+
+  .MuiTablePagination-caption {
+    font-size: 2.4em;
+  }
+
+  .MuiDataGrid-colCellCheckbox, .MuiDataGrid-cellCheckbox {
+    svg {
+      color: #f4ede8;
+    }
+    color: #f4ede8;
+  }
+  }
+
+  .MuiDataGrid-columnSeparator {
+    svg {
+      color: #000;
+    }
+    color: #000;
+  }
 `;
